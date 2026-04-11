cad4676eb1e2ef05fe12cb105f11a50e07b4ac56
sujal872 <262487629+sujal872@users.noreply.github.com>
2026-04-11 11:36:37 +0530

    Add domain lookup fetures using socket

---
+++ core/domain_info.py
@@ -0,0 +1,32 @@
+import socket
+
+def domain():
+    domain = input("Enter a domain: ").strip()
+
+    try:
+        ipv4 = socket.gethostbyname(domain)
+        try:
+            ipv6 = socket.getaddrinfo(domain, None, socket.AF_INET6)[0][4][0]
+        except:
+            ipv6 = "Not available"
+        print("\n===== DOMAIN LOOKUP =====")
+
+        print(f"Domain: {domain}")
+        print(f"IPv4: {ipv4}")
+        print(f"IPv6: {ipv6}")
+
+        print("\n===== REVERSE LOOKUP =====")
+
+        # Add timeout to avoid long delay
+        socket.setdefaulttimeout(2)
+
+        try:
+            hostname = socket.gethostbyaddr(ipv4)[0]
+        except socket.herror:
+            hostname = "Reverse lookup failed"
+
+        print(f"IP: {ipv4}")
+        print(f"Hostname: {hostname}")
+
+    except socket.gaierror:
+        print("Could not resolve domain.")
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from utils import banner
+import socket
+from core import domain_info
 
+def main():
+    domain_info.domain()
+    
+    
 if __name__ == "__main__":
     banner.banner()
+    main()
